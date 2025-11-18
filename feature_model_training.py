39ad48fd6b85e88fe53e936f50375c9b49d7fec1
Orlin Kralov <orlinkralov@gmail.com>
2025-11-18 13:41:45 +0200

    feature model training cleanup

--- feature_model_training.py
+++ feature_model_training.py
@@ -1,25 +1,14 @@
 import pandas as pd
 
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report, confusion_matrix
 
 df = pd.read_csv('./datasets/phishing_url_features_combined_1.csv')
 
-# This is just to check that the dataframe has loaded correctly
-# print("Shape: ", df.shape)
-# print("\nColumn names: ")
-# print(df.columns.tolist())
-# print("\nFirst columns: ")
-# print(df.head())
-
 target_column = 'phishing'
 X = df.drop(target_column, axis=1)
 y = df[target_column]
-
-# Quick check if our dataset is balanced
-# print("Class distribution: ")
-# print(y.value_counts())
 
 model = RandomForestClassifier(
     n_estimators=50,
